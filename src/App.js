b7b7dc3f68fb966f6c77855415f231b5f2ae052d
MvexD <muwiewow@gmail.com>
2021-07-01 14:54:39 +0200

    xD

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import InputData from "./Components/InputData";
+
+function App() {
+  return (
+    <div className="App">
+      <InputData />
+    </div>
+  );
+}
+
+export default App;
